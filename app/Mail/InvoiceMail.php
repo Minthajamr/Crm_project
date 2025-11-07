fd37c69e25df072a85a7ae894bdaefe873ee8ccd
Minthaj <minthajameer@gmail.com>
2025-11-07 16:58:23 +0530

    Create Welcome mail & Invoice mail

---
+++ app/Mail/InvoiceMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Invoice;
+
+class InvoiceMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $invoice;
+
+    public function __construct(Invoice $invoice)
+    {
+        $this->invoice = $invoice;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Invoice #' . $this->invoice->invoice_number,
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.invoice',
+        );
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}
